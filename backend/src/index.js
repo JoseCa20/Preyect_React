b451c95245fb4d1dfbf37748187d5b8cc49dd5eb
Joseca20 <crisskcres1720@gmail.com>
2023-11-06 10:10:59 -0500

    Se realiza la conexión backend con frontend, todo funciona!!!

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(require('./routes/routes'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(cors({
     origin: "*",
@@ -24,4 +24,11 @@ app.listen(app.get('Port'), () => {
     console.log('escuchando por el puerto:', app.get('Port'));
 })
 
-app.get("/productos")
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+})
